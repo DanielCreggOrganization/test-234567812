3d68242b4f821d41e80b7eebbb322cf4bbc02ede
Katarzyna Krakowska <g00364903@gmit.ie>
2020-01-20 17:28:36 +0000

    added for loop

--- src/ie/gmit/packagename/HelloWorld.java
+++ src/ie/gmit/packagename/HelloWorld.java
@@ -4,7 +4,14 @@ class HelloWorld {
 
     public static void main(String[] args) {
         // Print Hello World! to the console.
-        System.out.println("Hello World!");
+        System.out.println("Hello Kasia!");
+        System.out.println("sysout works");
+        System.out.println("main works too");
+        System.out.println("shift+alt+f to beautify code");
+        
+        for (int i = 0; i < 10; i++) {
+        System.out.println("Hello");
+        }
     }
 
 }
