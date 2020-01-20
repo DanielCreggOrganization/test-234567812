46a76f6418f50e39cf0a74373cd597e9ba5104a3
Katarzyna Krakowska <g00364903@gmit.ie>
2020-01-20 17:32:57 +0000

    for loop

--- src/ie/gmit/packagename/HelloWorld.java
+++ src/ie/gmit/packagename/HelloWorld.java
@@ -9,6 +9,8 @@ class HelloWorld {
         System.out.println("main works too");
         System.out.println("shift+alt+f to beautify code");
         
+        System.out.println("to save: source control, click plus next tpo file, click tick button, dots to opne list and press push ");
+
         for (int i = 0; i < 10; i++) {
         System.out.println("Hello");
         }
