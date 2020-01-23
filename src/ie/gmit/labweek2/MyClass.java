ff3a28fe630173aaf5bde6b3ca2e1a4944bc6ad3
Katarzyna Krakowska <g00364903@gmit.ie>
2020-01-23 14:31:48 +0000

    MyClass

---
+++ src/ie/gmit/labweek2/MyClass.java
@@ -0,0 +1,16 @@
+package ie.gmit.labweek2;
+
+class MyClass {
+
+    public static void main(String[] args) {
+
+        for (int i = 0; i < 10; i++) {
+            myMethod();
+        }
+    }
+
+    public static void myMethod() {
+        System.out.println("hi");
+
+    }
+}
